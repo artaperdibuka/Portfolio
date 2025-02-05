9ff02ed71b4329b1e76266d38be717b31b4e8296
artaperdibuka <220307144.b@uni-prizren.com>
2025-02-05 20:06:09 +0100

    react-portfolio-web

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
             <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>JohhnDoe</h5>
-            <a href="https://m.me/artaperdibukaa" target="_blank"> Send a message</a>
+            <a href="https://m.me/johnnydoee" target="_blank"> Send a message</a>
           </article>
           <article className='contact__option'>
             <BsWhatsapp className='contact__option-icon'/>
